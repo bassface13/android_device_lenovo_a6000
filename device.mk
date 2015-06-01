f0392ae5e1902e920f32d3ac6707d807eb2a6c55
bassface13 <manasm678@gmail.com>
2015-06-01 16:10:02 +0530

    rebirth

--- device.mk
+++ device.mk
@@ -18,178 +18,21 @@ LOCAL_PATH := device/lenovo/a6000
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
-$(call inherit-product-if-exists, vendor/lenovo/a6000/a6000-vendor.mk)
+# call the proprietary setup
+$(call inherit-product, vendor/lenovo/a6000/a6000-vendor.mk)
 
-# Ramdisk
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/ramdisk/etc/dpm/fdMgr/fd.conf:system/etc/dpm/fdMgr/fd.conf
+# call the gps config appropriate for this device
+$(call inherit-product, device/common/gps/gps_us_supl.mk)
 
-# Screen density
 PRODUCT_AAPT_CONFIG := normal hdpi xhdpi
-PRODUCT_AAPT_PREF_CONFIG := xhdpi
+PRODUCT_AAPT_PREF_CONFIG := hdpi
 
-DEVICE_PACKAGE_OVERLAYS := \
-    $(LOCAL_PATH)/overlay
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/init.environ.rc:root/init.environ.rc \
+    $(LOCAL_PATH)/init.qti.carrier.rc:root/init.qti.carrier.rc \
+    $(LOCAL_PATH)/init.qcom.usb.rc:root/init.qcom.usb.rc \
+    $(LOCAL_PATH)/init.qcom.rc:root/init.qcom.rc \
+    $(LOCAL_PATH)/init.recovery.qcom.rc:root/init.recovery.qcom.rc \
+    $(LOCAL_PATH)/init.target.rc:root/init.target.rc
 
-PRODUCT_PACKAGES += \
-    gralloc.msm8916 \
-    copybit.msm8916 \
-    hwcomposer.msm8916 \
-    memtrack.msm8916 \
-    power.msm8916
-
-PRODUCT_PACKAGES += \
-    audio.msm8916 \
-    audio_policy.msm8916
-
-PRODUCT_PACKAGES += \
-    audiod \
-    audio.primary.msm8916 \
-    audio.a2dp.default \
-    audio.usb.default \
-    audio.r_submix.default \
-    libaudio-resampler \
-    tinymix \
-    libqcomvisualizer \
-    libqcomvoiceprocessing
-
-# ANT+
-PRODUCT_PACKAGES += \
-    AntHalService \
-    com.dsi.ant.antradio_library \
-    libantradio
-
-# Camera
-PRODUCT_PACKAGES += \
-    camera.msm8916 \
-    libmm-qcamera
-
-# Filesystem
-PRODUCT_PACKAGES += \
-    setup_fs
-
-# Qcom SoftAP & wifi
-PRODUCT_PACKAGES += \
-    libQWiFiSoftApCfg
-
-# lenovo
-PRODUCT_PACKAGES += \
-    charge_only_mode
-
-# Misc
-PRODUCT_PACKAGES += \
-    libxml2
-
-# OMX
-PRODUCT_PACKAGES += \
-    libdashplayer \
-    libOmxCore \
-    libOmxVdec \
-    libOmxVdecHevc \
-    libOmxVenc \
-    libstagefrighthw 
-
-PRODUCT_PACKAGES += \
-    wlan_module_symlink \
-    wlan_persist_symlink \
-    wcnss_service
-
-PRODUCT_PACKAGES += \
-    librs_jni \
-    com.android.future.usb.accessory
-
-# Ebtables
-PRODUCT_PACKAGES += \
-    ebtables \
-    ethertypes \
-    libebtc
-
-# FM
-PRODUCT_PACKAGES += \
-    FM2 \
-    FMRecord \
-    libqcomfm_jni \
-    qcom.fmradio
-
-# Keystore
-PRODUCT_PACKAGES += \
-    keystore.msm8916
-
-# Lights
-PRODUCT_PACKAGES += \
-    lights.msm8916
-
-# GPS
-PRODUCT_PACKAGES += \
-    gps.msm8916
-
-# CRDA
-PRODUCT_PACKAGES += \
-    crda \
-    linville.key.pub.pem \
-    regdbdump \
-    regulatory.bin
-
-# Misc
-PRODUCT_PACKAGES += \
-    curl \
-    libbson \
-    libcurl \
-    tcpdump
-
-# Wifi
-PRODUCT_PACKAGES += \
-    p2p_supplicant_overlay.conf \
-    wpa_supplicant_overlay.conf
-
-# Wifi
-PRODUCT_PACKAGES += \
-    hostapd.accept \
-    hostapd_default.conf \
-    hostapd.deny \
-    hostapd \
-    wpa_supplicant \
-    wpa_supplicant.conf \
-    dhcpcd.conf
-
-PRODUCT_PACKAGES += \
-    WCNSS_qcom_wlan_factory_nv.bin
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sf.lcd_density=240
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.sys.logkit.ctrlcode=0 \
-    ro.usb.mtp=0x2e82 \
-    ro.usb.mtp_adb=0x2e76 \
-    ro.usb.ptp=0x2e83 \
-    ro.usb.ptp_adb=0x2e84 \
-    ro.usb.bpt=0x2ec1 \
-    ro.usb.bpt_adb=0x2ec5 \
-    ro.usb.bpteth=0x2ec3 \
-    ro.usb.bpteth_adb=0x2ec6 \
-    persist.audio.calfile0=/etc/acdbdata/Bluetooth_cal.acdb \
-    persist.audio.calfile1=/etc/acdbdata/General_cal.acdb \
-    persist.audio.calfile2=/etc/acdbdata/Global_cal.acdb \
-    persist.audio.calfile3=/etc/acdbdata/Handset_cal.acdb \
-    persist.audio.calfile4=/etc/acdbdata/Hdmi_cal.acdb \
-    persist.audio.calfile5=/etc/acdbdata/Headset_cal.acdb \
-    persist.audio.calfile6=/etc/acdbdata/Speaker_cal.acdb \
-    persist.sys.qc.sub.rdump.max=3 \
-    mdc_initial_max_retry=10 \
-    drm.service.enabled=true \
-    ro.media.enc.aud.fileformat=amr \
-    ro.media.enc.aud.codec=amrnb \
-    ro.gps.agps_provider=1
-
-# Media
-PRODUCT_PROPERTY_OVERRIDEs += \
-    media.stagefright.use-awesome=true
-
-# Inhert dalvik heap values from aosp
 $(call inherit-product, frameworks/native/build/phone-hdpi-2048-dalvik-heap.mk)
-
-
-$(call inherit-product-if-exists, hardware/qcom/msm8x16/msm8x16.mk)
-
-
